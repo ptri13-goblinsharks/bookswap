6020f7f01dbb6ecdf29e3754334d02b78f0721d8
PavelKrapivin <pavelkrapivin@Pavels-MacBook-Air.local>
2024-01-11 20:53:49 -0500

    first commit

---
+++ client/components/HomeNavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HomeNavBar() {
+  return (
+    <nav className='home-navbar'>
+      <h2>Home</h2>
+      <h2>My Library</h2>
+      <button>Log out</button>
+    </nav>
+  );
+}
+
+export default HomeNavBar;
