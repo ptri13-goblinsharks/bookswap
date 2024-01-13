7dd1f1de013d7c51077e1294da90bfc80876aaf1
Mike Basta <mikebasta@gmail.com>
2024-01-13 12:38:35 -0800

    descriptions addeD

--- client/components/MyBooks.js
+++ client/components/MyBooks.js
@@ -1,3 +1,15 @@
+/**
+ * ************************************
+ *
+ * @module  MyBooks.js
+ * @author
+ * @date
+ * @description display componenent that renders user library
+ *
+ * ************************************
+ */
+
+
 import React from 'react'
 
 const MyBooks = () => {
