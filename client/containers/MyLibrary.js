296fa761abef92f4af9664207ea82afecf27cec8
Mike Basta <mikebasta@gmail.com>
2024-01-12 21:38:23 -0800

    added state to MyLibrary container and passed appropriately to child components

--- client/containers/MyLibrary.js
+++ client/containers/MyLibrary.js
@@ -1,25 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import HomeNavBar from './HomeNavBar';
-import AddBook from '../components/AddBook';
+// import AddBook from '../components/AddBook';
 import HomeSearchBar from '../components/HomeSearchBar';
 import BorrowedBooks from '../components/BorrowedBooks';
 import CheckedOut from '../components/CheckedOut';
+import MyBooks from '../components/MyBooks';
+import { useSelector } from 'react-redux';
 
 const MyLibrary = () => {
     // pull library data from DB here and pass to search bar as props.
     // reuse HomeSearchBar.js instead of AddBook.js
-    let libraryBooks;
+
+    const [myLibraryBooks, setMyLibraryBooks] = useState('');
+    const [globalLibraryBooks, setGlobalLibraryBooks] = useState('');
+    
+    //call to personal library 
     fetch('')
     .then (data => {
-        libraryBooks = data;
+        setMyLibraryBooks(data);
+    })
+    //call to global library
+    fetch('')
+    .then (data => {
+        setGlobalLibraryBooks(data);
     })
 
   return (
     <div>
         <HomeNavBar />
-        <HomeSearchBar books={libraryBooks}/>
-        <BorrowedBooks />
-        <CheckedOut />
+        <HomeSearchBar books={globalLibraryBooks}/>
+        <MyBooks books={myLibraryBooks}/>
+        <BorrowedBooks books={myLibraryBooks} />
+        <CheckedOut books={myLibraryBooks}/>
     </div>
 
   )
