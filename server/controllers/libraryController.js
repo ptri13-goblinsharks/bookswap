388d313dab1743cc0763f5c5f4f80a0b6c67d0cb
Hubbisand <Benjamin.Reid.Jackson@gmail.com>
2024-01-15 17:22:27 -0500

    Commit for Regular Merge with Dev

--- server/controllers/libraryController.js
+++ server/controllers/libraryController.js
@@ -1,13 +1,14 @@
-const db = require
+const models = require('../models/models.js')
 
 const libraryController = {};
 
-libraryController.addBook = (req, res, next) => {
-
+libraryController.addBook = async (req, res, next) => {
+  await models.Book.create(req.body);
+  return next();
 }
 
 libraryController.deleteBook = (req,res,next) => {
-
+  
 }
 
 libraryController.updateBook = (req, res,next) => {
