61bf27f811fc307deee583b7da455fca2527e457
imdarren93 <se.darrenim@gmail.com>
2024-01-17 18:51:01 -0800

    finished addbook middleware and aggregate method in librarycontroller

--- server/routes/library.js
+++ server/routes/library.js
@@ -7,18 +7,27 @@ const sessionController = require('../controllers/sessionController')
 
 const router = express.Router();
 
-router.post('/findBook',
+router.post('/action/findBook',
   libraryController.checkLibrary,
   apiController.checkApi,
   (req, res) => res.status(200).json(res.locals.bookData)
 );
 
-router.post('/addBook',
+router.post('/action/addBook',
   libraryController.addToGlobalLibrary,
   sessionController.isLoggedIn,
   userController.addToUserLibrary,
   (req, res) => res.status(200).json(res.locals.userLibrary)
 )
+
+router.get('/action/globalLibrary',
+  libraryController.getAllBooks,
+  (req, res) => res.status(200).json(res.locals.globalLibrary)
+)
+
+router.post('/action/retrieveBook', libraryController.retrieveBook, (req, res) => {
+  res.status(200).json(res.locals.foundBooks);
+})
 
 // router.get('/getLibrary/:username',
 //   libraryController.getUserLibrary,
