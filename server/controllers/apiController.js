6c101c89aa6b24daa1e6de6ed4325d26e8011308
imdarren93 <se.darrenim@gmail.com>
2024-01-17 17:48:13 -0800

    working through routes and controllers. updated books key on userSchema

---
+++ server/controllers/apiController.js
@@ -0,0 +1,35 @@
+const db = require('../models/models');
+
+
+const apiController = {};
+
+apiController.checkApi = async (req, res, next) => {
+    try {
+        if (res.locals.title){
+            const bookTitle = res.locals.title.replaceAll(' ', '+');
+            const response = await fetch('https://openlibrary.org/search.json?title=' + bookTitle);
+            const data = await response.json();
+            res.locals.bookData = {olKey : data.docs[0].key, title : data.docs[0].title, author : data.docs[0].author_name};
+            return next();
+        }
+        else {
+            return next();
+        }
+    } catch (error) {
+        console.log ('Error in apiController fetch request')
+    }
+}
+
+apiController.addToGlobalLibrary = async (req, res, next) => {
+    try {
+        const olWorkNumber = req.body;
+        const response = await fetch('https://openlibrary.org/works/' + olWorkNumber + '.json');
+        const data = await response.json();
+        
+    } catch (error) {
+        console.log('Error in addToGlobalLibrary middleware')
+    }
+}
+
+module.exports = apiController;
+
